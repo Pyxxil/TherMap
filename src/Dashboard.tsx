1010e8cae3352e59f451567625e0093bbf6500a3
Kimberley <kim@evans-parker.com>
2021-07-31 21:50:56 +1200

    Move functions to util and constants

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,28 +1,17 @@
 import React, { useContext, useEffect, useState } from "react";
 
 import { Location, LocationContext } from "./Location";
-import { EuclideanDistance } from './Utils';
+import { EuclideanDistance, getFlameLeft } from './Utils';
 
 import Tree from "./img/tree.png";
-import Fire1 from "./img/fire1.jpg";
-import Fire2 from "./img/fire2.jpg";
 import "./styles.css";
+import { fireLocations } from "./constants";
 
 interface Props {
   destination?: Location;
 }
 
 const Dashboard: React.FC<Props> = (props) => {
-
-  function getFlameSize() {
-    return (Math.round(Math.random() * 20) + 40).toString();
-  }
-  function getFlameImage() {
-    return Math.random() > 0.5 ? Fire1 : Fire2;
-  }
-  function getFlameLeft(left: string, size: string) {
-    return (parseInt(left) - parseInt(size) / 2).toString() + "px"
-  }
 
   const { location } = useContext(LocationContext);
   const [distance, setDistance]= useState(0);
@@ -43,30 +32,7 @@ const Dashboard: React.FC<Props> = (props) => {
     
   }, [location, props.destination]);
   
-  let temperature = 0; // 0 cold, 100 hot
-  
-  const fireLocations = [ // image, bottom, left, size
-    [getFlameImage(), "400px", "140", getFlameSize()], 
-    [getFlameImage(), "380px", "220", getFlameSize()],
-    [getFlameImage(), "320px", "80", getFlameSize()], 
-    [getFlameImage(), "305px", "20", getFlameSize()],  
-    [getFlameImage(), "300px", "220", getFlameSize()], 
-    [getFlameImage(), "290px", "320", getFlameSize()], 
-    [getFlameImage(), "250px", "40", getFlameSize()], 
-    [getFlameImage(), "230px", "130", getFlameSize()], 
-    [getFlameImage(), "205px", "350", getFlameSize()], 
-    [getFlameImage(), "174px", "235", getFlameSize()], 
-    [getFlameImage(), "160px", "30", getFlameSize()], 
-    [getFlameImage(), "140px", "310", getFlameSize()], 
-    [getFlameImage(), "140px", "90", getFlameSize()], 
-    [getFlameImage(), "60px", "70", getFlameSize()],
-    [getFlameImage(), "40px", "140", getFlameSize()], 
-    [getFlameImage(), "40px", "270", getFlameSize()], 
-    [getFlameImage(), "30px", "20", getFlameSize()], 
-    [getFlameImage(), "10px", "210", getFlameSize()], 
-    [getFlameImage(), "5px", "330", getFlameSize()], 
-    [getFlameImage(), "-10px", "100", getFlameSize()]
-  ]
+  let temperature = 100; // 0 cold, 100 hot
 
   if (location && props.destination && originalDistance) {
     return (
@@ -86,10 +52,10 @@ const Dashboard: React.FC<Props> = (props) => {
       <img src={Tree} className="tree" 
         style={{filter: `sepia(${(temperature - 50) * 2})`, WebkitFilter: `sepia(${(temperature - 50) * 2}%)`}} />
 
-      {fireLocations.map((location: string[]) => {
-        return <img src={location[0]} style={{bottom: location[1], left: getFlameLeft(location[2], location[3]), width: location[3] + "px"}} className="fire" key={location[0] + location[1]} />
+      {temperature > 50 && fireLocations.map((location: string[]) => {
+        return <img src={location[0]} style={{bottom: location[1], left: getFlameLeft(location[2], location[3]), width: location[3] + "px"}} className="fire" key={location[1] + location[2]} />
       })}
-      
+
       </div>
     );
   }
--- src/Utils.ts
+++ src/Utils.ts
@@ -1,4 +1,6 @@
 import { Location } from './Location';
+import Fire1 from "./img/fire1.jpg";
+import Fire2 from "./img/fire2.jpg";
 
 export const EuclideanDistance = (source: Location, target: Location): number => {
   const a = (source.lat - target.lat);
@@ -8,4 +10,16 @@ export const EuclideanDistance = (source: Location, target: Location): number =>
   const c = Math.sqrt(a * a + b * b);
 
   return c * 111;
+}
+
+export function getFlameSize() {
+  return (Math.round(Math.random() * 20) + 40).toString();
+}
+
+export function getFlameImage() {
+  return Math.random() > 0.5 ? Fire1 : Fire2;
+}
+
+export function getFlameLeft(left: string, size: string) {
+  return (parseInt(left) - parseInt(size) / 2).toString() + "px"
 }
